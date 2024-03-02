2a6b5065ea0ccefc3dcbe08c1226a62929c44689
Alperen Aktuğ <aktugalperen9@gmail.com>
2024-03-02 13:00:32 +0300

    data-type

---
+++ app.js
@@ -0,0 +1,33 @@
+// let age: number = 23;
+// let firstname: string = "John";
+// let lastname: string = "Cena";
+// let isUpdated: boolean = true;
+// function display (id:number , name:string)
+// {
+//   console.log("Id = " + id + "Name = " + name);
+//}
+// ----------------Data Type:-----------------------
+// 1- NUMBER:
+// let first:number = 123; //number
+// let second: number = 0x37CF ; // hexadecimal
+// let third: number = 0o377; // octal
+// let fourth: number = 0b111001; // binary
+// console.log(first);
+// console.log(second);
+// console.log(third);
+// console.log(fourth);
+// 2- STRİNG :
+// let  employeefirstName: string = "Muro";
+// let  employeefirstName: string = 'Muro';
+// console.log(employeefirstName);
+// let employeeName: string = "John Cena";
+// let employeeDept: string = "Under Taker";
+// let employeeDesc1: string = employeeName + " works in the " + employeeDept + " department.";
+// console.log(employeeDesc1);
+// let employeeDesc2: string = `${employeeName} works in the ${employeeDept} department.`
+// console.log(employeeDesc2)
+// 3- Boolean
+// let isPresent1: boolean = true;
+// let isPresent2: boolean = false;
+// console.log(isPresent1);
+// console.log(isPresent2);
